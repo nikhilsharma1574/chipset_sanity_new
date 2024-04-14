b5b81fb3e3a96aa93a457ef668159c71a795dd1a
Nikhil Sharma <nikhilsharma1574@gmail.com>
2024-04-14 10:38:31 +0530

    Navbar

--- layouts/Navbar.tsx
+++ layouts/Navbar.tsx
@@ -2,104 +2,106 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from "../public/assets/logo/64x-black-logo.png"
-
-import { Menu } from 'lucide-react'
 import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from 'lucide-react';
 
 const Navbar = () => {
-
   const LINKS = [
-    {
-      label: "Home",
-      link: "/"
-    },
-    {
-      label:"About us",
-      link: "/about"
-    },
-    {
-      label:"Team",
-      link: "/team"
-    },
-    {
-      label:"Notice",
-      link: "/notice"
-    },
-    {
-      label:"Gallery",
-      link: "/gallery"
-    },
-    {
-      label:"Event",
-      link: "/events"
-    },
-    {
-      label:"Contact Us",
-      link: "/contact"
-    },
-  ]
-  return (
-    <section id="" className="sticky top-0 z-40 backdrop-blur">
-      <nav className ="md:w-full transition-all delay-10 h-[4rem] sm:h-[7rem] md:h-[7rem] px-4">
-        <div className ="max-w-screen-xl flex flex-wrap items-center justify-between  mx-auto h-full">
-            <Link href="/" className="flex justify-center items-center">
-              <div className='flex'>
-                <div className=''>
-                <Image src={logo} height={100} width={100} alt='Logo' className='w-10 md:w-16 md:p-1'/>
-                </div>
-              <div className=''>
-                <p className='font-bold text-[19px] md:text-[24px] lg:text-[29px]'>CHiPSET</p>
-                <p className='font-semibold uppercase text-[6px] md:text-[8px] lg:text-[10px] text-[#ff5234]'>A Technical Community</p>
-              </div>
-              </div>
-          </Link>
-            <Sheet>
-              <SheetTrigger className='md:hidden'><Menu></Menu></SheetTrigger>
-              <SheetContent className=''>
-                <SheetHeader className=''>
-                  <SheetTitle>Menu</SheetTitle>
-                  <SheetDescription className='w-full h-full'>
-                    {
-                      LINKS.map((item, idx) => (
-                        <Link href={item.link} key={idx}>
-                          <ul className='font-heading font-semibold uppercase sm:text-xs text-[12px] text-[#ff5234] my-4'>
-                            <li>
-                              {item.label}
-                            </li>
-                          </ul>
-                        </Link>
-                      ))
-                    }
-                  </SheetDescription>
-                </SheetHeader>
-              </SheetContent>
-            </Sheet>
+         {
+           label: "Home",
+           link: "/"
+         },
+         {
+           label:"About us",
+           link: "/about"
+         },
+         {
+           label:"Team",
+           link: "/team"
+         },
+         {
+           label:"Notice",
+           link: "/notice"
+         },
+         {
+           label:"Gallery",
+           link: "/gallery"
+         },
+         {
+           label:"Event",
+           link: "/events"
+         },
+         {
+           label:"Contact Us",
+           link: "/contact"
+         },
+       ]
 
-          <div className ="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul style={{ mixBlendMode: 'difference' }} className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              {
-                LINKS.map((item, idx) => (
-                  <div className='' key={idx}>
-                  <Link href={item.link} key={idx} style={{ mixBlendMode: 'difference' }}  className='font-heading block py-2 pl-3 pr-4 sm:text-[12px] lg:text-[16px] rounded  md:hover:text-[#ff5234] md:p-0' >                      
-                          {item.label}
-                  </Link>
+    return (
+        <section id="" className="sticky top-0 z-40 backdrop-blur mix-blend-exclusion">
+            <header className="w-full p-4 md:px-16 py-8 lg:max-w-7xl lg:mx-auto flex items-center justify-between">
+                <div>
+                  <Link href="/" className="flex justify-center items-center ">
+                  <div className='flex'>
+                  <div className=''>
+                    <Image src={logo} height={100} width={100} alt='Logo' className='w-10 md:w-16 md:p-1'/>
                   </div>
-                ))
-              }              
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </section>
-  )
-}
+                  <div className=''>
+                    <p className='font-bold text-card text-[19px] md:text-[24px] lg:text-[29px]'>CH<span className='text-orange-600 mix-blend-color-dodge text-pretty' >i</span>PSET</p>
+                    <p className='font-semibold text-accent uppercase text-[6px] md:text-[8px] lg:text-[10px] text-[#ff5234] mix-blend-none z-40'>A Technical Community</p>
+                  </div>
+                </div>
+          </Link>
+                </div>
+                <div className="gap-4 hidden lg:flex">
+                    {LINKS.map((item, idx) => {
+                        return (
+                            <Link
+                                key={idx}
+                                href={item.link}
+                                className="text-secondary transition-all duration-300  hover:text-primary"
+                            >
+                                {item.label}
+                            </Link>
+                        );
+                    })}
+                </div>
+                <div className="lg:hidden">
+                    <Sheet>
+                        <SheetTrigger>
+                        <Menu></Menu>
+                            BUTTON
+                        </SheetTrigger>
+                        <SheetContent side={"top"} className="bg-primary text-slate-200" >
+                            <SheetHeader>
+                                <SheetTitle className="text-slate-200">Surendar PD</SheetTitle>
+                                <SheetDescription className="flex flex-col gap-4">
+                                    {LINKS.map((item, idx) => {
+                                        return (
+                                            <Link
+                                                key={idx}
+                                                href={item.link}
+                                                className="hover:text-secondary transition-all duration-300 text-lg"
+                                            >
+                                                {item.label}
+                                            </Link>
+                                        );
+                                    })}
+                                </SheetDescription>
+                            </SheetHeader>
+                        </SheetContent>
+                    </Sheet>
+                </div>
+            </header>
+        </section>
+    );
+};
 
-export default Navbar
+export default Navbar;
