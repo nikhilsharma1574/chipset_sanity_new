d019e8d1c8603586371e69eb056905cd3b6688c6
danesh0611 <messidhanesh2006@gmail.com>
2025-07-08 14:50:35 +0530

    videos changed to sanity databse

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -5,3 +5,12 @@ export const projectsQuery= groq`*[_type == 'project']{...,"mainImage":mainImage
 export const teamQuery = groq`*[_type == 'team' && title != null] | order(title desc){...,"mainImage":mainImage.asset->url,"alt":mainImage.alt}`;
 export const noticeQuery = groq`*[_type == "notice"]{_id,title,desc,"attachmentURL" : attachment.asset->url}`;
 export const galleryQuery = groq`*[_type == 'gallery']{...,"mainImage":mainImage[]{'url':asset->url}}`; 
+export const aboutVideosQuery = groq`
+  *[_type == "aboutVideos"][0]{
+    "aboutUsVideo": aboutUsVideo.asset->url,
+    "howItsGoingVideo": howItsGoingVideo.asset->url,
+    "howItStartedVideo": howItStartedVideo.asset->url,
+    "futureVisionVideo": futureVisionVideo.asset->url,
+    "landingpagevideo": landingpagevideo.asset->url
+  }
+`;
--- sanity/schema.ts
+++ sanity/schema.ts
@@ -4,6 +4,7 @@ import events from './schemaTypes/events'
 import gallery from './schemaTypes/gallery'
 import team from './schemaTypes/team'
 import noticeFile from './schemaTypes/noticeFile'
+import videos from './schemaTypes/videos'
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [hero,events,team,noticeFile,gallery],
+  types: [hero,events,team,noticeFile,gallery,videos],
 }
